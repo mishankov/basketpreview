612bf55a38d8a404fcd38fa0b1dd21f5287b69f2
mishankov <mishankov@mail.com>
2017-04-21 19:47:23 +0300

    mb finaly proper ignore

--- basketpreview/settings.py
+++ basketpreview/settings.py
@@ -23,7 +23,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = 'a6cfhre1tx%+ck!xz$%sizjcy&v#d$z4igjv(!_o#2lob-86!4'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = ['*']
 
@@ -120,3 +120,4 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/1.10/howto/static-files/
 
 STATIC_URL = '/static/'
+# STATIC_ROOT = '/root_static/'
